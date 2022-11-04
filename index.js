3ab29e68cef1e16436dd5d123d43aaa34807c6c3
Kurt Roekle <kroekle@gmail.com>
2022-11-04 17:07:42 -0500

    still not expecting it to work

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-// const github = require('@actions/github');
+const github = require('@actions/github');
 const httpm = require('@actions/http-client')
 
 try {
