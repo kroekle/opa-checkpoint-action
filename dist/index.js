159409ce173140039ad5014eef30a6039a9d7b47
Kurt Roekle <kroekle@gmail.com>
2022-11-04 18:18:53 -0500

    await this

--- dist/index.js
+++ dist/index.js
@@ -9689,20 +9689,21 @@ const github = __nccwpck_require__(3134);
 const httpm = __nccwpck_require__(6227)
 
 try {
-  const url = `${core.getInput('tenant')}/v1/data/systems/${core.getInput('system-id')}/${core.getInput('rule')}?publish_decision=true`
-  console.log(`This is what I'm calling: ${url}`);
-  console.log(`Do I have a token: ${core.getInput('api-token').startsWith("f50")}`);
+//   const url = `${core.getInput('tenant')}/v1/data/systems/${core.getInput('system-id')}/${core.getInput('rule')}?publish_decision=true`
+//   console.log(`This is what I'm calling: ${url}`);
+//   console.log(`Do I have a token: ${core.getInput('api-token').startsWith("f50")}`);
   console.log(`Just a little: ${core.getInput('api-token').substring(0,10)}`);
   core.setOutput("messages", ["I see you", "but do you see me"]);
   // Get the JSON webhook payload for the event that triggered the workflow
 //   const payload = JSON.stringify({input:github.context.payload}, undefined, 2)
 //   console.log(`The event payload: ${payload}`);
 
-  const http = new httpm.HttpClient();
-  const additionalHeaders = {
-     "Authorization": `Bearer ${core.getInput('api-token')}`
-  }
-  const jsonObj = http.postJson(url, {input:github.context.payload}, additionalHeaders);
+//   const http = new httpm.HttpClient();
+//   const additionalHeaders = {
+//      "Authorization": `Bearer ${core.getInput('api-token')}`
+//   }
+//   const jsonObj = await http.postJson(url, {input:github.context.payload}, additionalHeaders);
+    const jsonObj = getDecision();
 
   console.log(`body: ${jsonObj}`);
 
@@ -9710,10 +9711,17 @@ try {
   core.setFailed(error.message);
 }
 
-async function getDecision(url, body) {
-    const response = await fetch(url);
-    const json = await response.json();
-}
+async function getDecision() {
+    const url = `${core.getInput('tenant')}/v1/data/systems/${core.getInput('system-id')}/${core.getInput('rule')}?publish_decision=true`
+    console.log(`This is what I'm calling: ${url}`);
+   
+    const http = new httpm.HttpClient();
+    const additionalHeaders = {
+       "Authorization": `Bearer ${core.getInput('api-token')}`
+    }
+    const jsonObj = await http.postJson(url, {input:github.context.payload}, additionalHeaders);
+    return jsonObj;
+  }
 
 })();
 
--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const github = require('@actions/github');
 const httpm = require('@actions/http-client')
 
 try {
-  const url = `${core.getInput('tenant')}/v1/data/systems/${core.getInput('system-id')}/${core.getInput('rule')}?publish_decision=true`
-  console.log(`This is what I'm calling: ${url}`);
-  console.log(`Do I have a token: ${core.getInput('api-token').startsWith("f50")}`);
+//   const url = `${core.getInput('tenant')}/v1/data/systems/${core.getInput('system-id')}/${core.getInput('rule')}?publish_decision=true`
+//   console.log(`This is what I'm calling: ${url}`);
+//   console.log(`Do I have a token: ${core.getInput('api-token').startsWith("f50")}`);
   console.log(`Just a little: ${core.getInput('api-token').substring(0,10)}`);
   core.setOutput("messages", ["I see you", "but do you see me"]);
   // Get the JSON webhook payload for the event that triggered the workflow
 //   const payload = JSON.stringify({input:github.context.payload}, undefined, 2)
 //   console.log(`The event payload: ${payload}`);
 
-  const http = new httpm.HttpClient();
-  const additionalHeaders = {
-     "Authorization": `Bearer ${core.getInput('api-token')}`
-  }
-  const jsonObj = http.postJson(url, {input:github.context.payload}, additionalHeaders);
+//   const http = new httpm.HttpClient();
+//   const additionalHeaders = {
+//      "Authorization": `Bearer ${core.getInput('api-token')}`
+//   }
+//   const jsonObj = await http.postJson(url, {input:github.context.payload}, additionalHeaders);
+    const jsonObj = getDecision();
 
   console.log(`body: ${jsonObj}`);
 
@@ -24,7 +25,14 @@ try {
   core.setFailed(error.message);
 }
 
-async function getDecision(url, body) {
-    const response = await fetch(url);
-    const json = await response.json();
-}
+async function getDecision() {
+    const url = `${core.getInput('tenant')}/v1/data/systems/${core.getInput('system-id')}/${core.getInput('rule')}?publish_decision=true`
+    console.log(`This is what I'm calling: ${url}`);
+   
+    const http = new httpm.HttpClient();
+    const additionalHeaders = {
+       "Authorization": `Bearer ${core.getInput('api-token')}`
+    }
+    const jsonObj = await http.postJson(url, {input:github.context.payload}, additionalHeaders);
+    return jsonObj;
+  }
