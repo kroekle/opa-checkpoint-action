f9306785dc89c3dd94cc386c5d8fe0a32c57289b
Kurt Roekle <kroekle@gmail.com>
2022-11-04 17:06:17 -0500

    calling das (but not likely to work)

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const core = require('@actions/core');
 // const github = require('@actions/github');
-const http = require('@actions/http-client')
+const httpm = require('@actions/http-client')
 
 try {
-  const url = `${core.getInput('tenant')}/v1/data/systems/${core.getInput('system-id')}/${core.getInput('rule')}`
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const url = `${core.getInput('tenant')}/v1/data/systems/${core.getInput('system-id')}/${core.getInput('rule')}?publish_decision=true`
   console.log(`This is what I'm calling: ${url}`);
   core.setOutput("messages", ["I see you", "but do you see me"]);
   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
 //   console.log(`The event payload: ${payload}`);
+
+  const http = httpm.HttpClient();
+  http.requestOptions = {
+    headers: {
+        "Authorization": `Bearer ${core.getInput('tenant')}`
+    }
+  }
+  const jsonObj = http.postJson<any>(url, payload);
+  console.log(`body: ${jsonObj.body}`);
+
 } catch (error) {
   core.setFailed(error.message);
 }
