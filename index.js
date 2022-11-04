6fad586dc50d4ac175260c34882b7fb2508ff16f
Kurt Roekle <kroekle@gmail.com>
2022-11-04 15:27:42 -0500

    adding env variables

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
+const http = require('@actions/http-client')
 
 try {
+  const url = `${core.getInput('tenant')}/v1/data/systems/${core.getInput('system-id')}/${core.getInput('rule')}`
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  console.log(`This is what I'm calling: ${url}`);
+  core.setOutput("messages", ["I see you", "but do you see me"]);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
+}
+
+async function getDecision(url, body) {
+    const response = await fetch(url);
+    const json = await response.json();
 }
